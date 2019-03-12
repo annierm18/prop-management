0a2f70d46fa6e03b29d805aef1b9031c6cd0963d
annierm18 <annierm18@yahoo.com>
2019-03-11 19:10:16 -0700

    finnaly finished newsletter new form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -58,12 +58,20 @@ import React, { Component } from 'react';
     }
 
     export class FormImage extends Component {
+
+
+        handleSelectedImage = (event) => {
+            var image = document.getElementById('newsletter-new-image');
+            image.src = URL.createObjectURL(event.target.files[0]);
+        }
+
         render() {
             const { className, title, input, type, imageUrl} = this.props;
             return (
                 <div className={`${className} form-image`}>
                     <label className='form-image__title'>{title}</label>
-                    <input 
+                    <img 
+                        id='newsletter-new-image'
                         className='form-image__image'
                         src={imageUrl}
                     />
@@ -83,6 +91,7 @@ import React, { Component } from 'react';
                         name='file'
                         accepts='image/*'
                         value={undefined}
+                        onChange={this.handleSelectedImage}
                         />
                     {/* replace button/input goes here */}
                 </div>
