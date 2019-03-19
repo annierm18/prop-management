36cd5c8284346cb7a491890a7bf198175af08afa
annierm18 <annierm18@yahoo.com>
2019-03-18 17:47:32 -0700

    refactored newsletter components to take props for custom cases

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -5,6 +5,7 @@ import * as actions from '../../actions';
 import NewsletterBox from './newsletterBox';
 import NewsletterLatest from './newsletterLatest';
 
+
 class NewsletterDetail extends Component {
 
     componentDidMount() {
@@ -15,8 +16,8 @@ class NewsletterDetail extends Component {
         console.log(this.props.newsletterToEdit);
         return (
             <div className='newsletter-detail'>
-                <NewsletterBox/>
-                <NewsletterLatest/>
+                <NewsletterBox date={this.props.newsletterToEdit.date}/>
+                <NewsletterLatest {...this.props.newsletterToEdit}/>
             </div>
         )
     }
